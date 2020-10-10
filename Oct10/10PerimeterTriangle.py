5337316b0ecaa12ff262010633ae69a080990892
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-10-10 13:20:16 +0530

    Perimeter Of The Triangle

---
+++ Oct10/10PerimeterTriangle.py
@@ -0,0 +1,7 @@
+a = float(input('Enter first side  : '))
+b = float(input('Enter second side : '))
+c = float(input('Enter third side  : '))
+
+s = (a + b + c)
+
+print('The area of the triangle is ', s)
