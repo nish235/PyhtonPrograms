8e9c04367ba81113470460f2a1f5ed4c78cc6668
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-10-13 14:35:54 +0530

    Maximum Between 2 Numbers

---
+++ Oct13/MaximumBetween2.py
@@ -0,0 +1,11 @@
+print(" Maximum Between 2 Numbers ")
+
+a = int(input("Enter 1st number : "))
+b = int(input("Enter 2nd number : "))
+
+if a > b:
+    print(a, "is greater ")
+elif a < b:
+    print(b, "is greater ")
+else:
+    print("Both Are Equal")
