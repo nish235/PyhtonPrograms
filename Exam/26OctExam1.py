739bf474f9fee3d971945a66de7faac8da897a34
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-10-27 13:33:13 +0530

    Exam Correct Answers

---
+++ Exam/26OctExam1.py
@@ -0,0 +1,25 @@
+# Ques No 4
+print(1.7 % 2)
+
+# Ques No 12
+print(round(0.5) - round(-0.5))
+
+# Ques No 14
+a = 3
+b = 1
+# print(cmp(a, b))
+
+# Ques No 43
+import sys
+l1 = tuple()
+print(sys.getsizeof(l1), end="\n")
+
+# Ques No 45
+d1 = [10, 20, 30, 40, 50]
+d2 = [1, 2, 3, 4, 5]
+print(d1-d2)
+
+# Traceback (most recent call last):
+#  File "C:\Users\nishm\Documents\PythonProgramming\Exam\26OctExam1.py", line 20, in <module>
+#    print(d1-d2)
+# TypeError: unsupported operand type(s) for -: 'list' and 'list'
