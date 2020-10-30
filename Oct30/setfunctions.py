2b0abba31ee226bfcc6b364ba0cb1420152cd38f
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-10-30 12:09:35 +0530

    Functions Used In Sets

---
+++ Oct30/setfunctions.py
@@ -0,0 +1,26 @@
+# copy
+s = {10, 20, 30, 40}
+s.copy()
+print(s)
+
+# pop
+s = {10, 20, 30, 40}
+s.pop()
+print(s)
+
+# remove
+s = {10, 20, 30, 40}
+s.remove(30)
+print(s)
+
+# discard
+s = {10, 20, 30, 40}
+s.discard(20)
+print(s)
+s.discard(11)
+print(s)
+
+# clear
+s = {10, 20, 30, 40}
+s.clear()
+print(s)
