954192a4b90e37d149d052b49005d73e3da35b90
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-10-30 11:56:50 +0530

    Mathematical Operations On Sets

---
+++ Oct30/MathematicalOperationsOnSet.py
@@ -0,0 +1,24 @@
+
+# Union Operation
+x = {10, 20, 30, 40}
+y = {11, 12, 13, 14, 20, 30}
+u = x.union(y)
+print(u)
+
+# Intersection Operation
+x = {10, 20, 30, 40}
+y = {11, 12, 13, 14, 20, 30}
+i = x.intersection(y)
+print(i)
+
+# Difference Operation
+x = {10, 20, 30, 40}
+y = {11, 12, 13, 14, 20, 30}
+d = x.difference(y)
+print(d)
+
+# Symmetric Difference
+x = {10, 20, 30, 40}
+y = {11, 12, 13, 14, 20, 30}
+s = x.symmetric_difference(y)
+print(s)
