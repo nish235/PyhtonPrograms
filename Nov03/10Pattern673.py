f5c9e634322353d89a872d353ba10c18a0474f82
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-11-03 17:11:45 +0530

    Pattern Question no 673

---
+++ Nov03/10Pattern673.py
@@ -0,0 +1,13 @@
+n = 9
+a = n//2+1
+for x in range(1, n+1):
+    for y in range(1, n+1):
+        if y == a or y == n-a+1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    if x <= n/2:
+        a -= 1
+    else:
+        a += 1
+    print()
