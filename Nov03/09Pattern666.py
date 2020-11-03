a723bc16a6f6365c0dd110d84ef0669b78fab7e8
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-11-03 17:06:20 +0530

    Pattern Question no 666

---
+++ Nov03/09Pattern666.py
@@ -0,0 +1,12 @@
+n = 5
+a = 1
+b = n * 2 - 1
+for x in range(n, 0, -1):
+    for y in range(1, n * 2 + 1):
+        if y == a or y == b:
+            print(chr(x+64), end="")
+        else:
+            print(" ", end="")
+    a += 1
+    b -= 1
+    print()
