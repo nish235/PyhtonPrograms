eae7bcb6d0cfb7bb4d2747299acf63d8cbbe2802
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-11-05 16:53:29 +0530

    Pattern Program

---
+++ Nov05/06Pattern790.py
@@ -0,0 +1,10 @@
+n = 5
+c = n // 2 + 1
+for x in range(1, n + 1):
+    for y in range(1, n + 1):
+        if x == c or y == c:
+            print("x", end=" ")
+        else:
+            print("o", end=" ")
+    print()
+
