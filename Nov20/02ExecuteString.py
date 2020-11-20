6c75089a053d98e7f784a48ac06765e735d3c877
Nishant Shrivastava <70203949+nish235@users.noreply.github.com>
2020-11-20 15:03:20 +0530

    Assignment

---
+++ Nov20/02ExecuteString.py
@@ -0,0 +1,7 @@
+x = """
+def multiply(x,y):
+    return x*y
+
+print('Multiply of 2 and 3 is: ',multiply(2,3))
+"""
+exec(x)
---
+++ Nov20/03NumOfLocalVariables.py
@@ -0,0 +1,7 @@
+def local():
+    x = 1
+    y = 2
+    str1 = "ram"
+
+
+print(local.__code__.co_nlocals)
